6d669bf46316713fc076a6087b1672074fd72e05
matolapendos29 <90642408+matolapendos29@users.noreply.github.com>
2021-10-31 23:40:39 +0900

    upload
    abc

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn koko_related.wsgi --log-file -
---
+++ buku/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+admin.site.register(Buku)
+admin.site.register(Kategori)
+admin.site.register(Book)
+admin.site.register(Store)
